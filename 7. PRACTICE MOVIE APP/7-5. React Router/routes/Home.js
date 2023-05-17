20229e3ce98b92c0ef2305860916ddb343ae8b64
yubeenso <youbin0105@gmail.com>
2023-05-17 23:48:51 +0900

    chapter 7 commit 5

---
+++ 7. PRACTICE MOVIE APP/7-5. React Router/routes/Home.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import Movie from "../components/Movie"; // 경로 적을 때 조심해야 한다. . 1개만 적으면 현재 폴더인 routes에서 찾기 때문!
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMoives] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+      )
+    ).json();
+    setMoives(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
